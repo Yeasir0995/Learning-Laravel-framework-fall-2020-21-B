5f06f478969c2f9cc5a5202860618bdc0a060c89
Yeasir0995 <40072145+Yeasir0995@users.noreply.github.com>
2020-12-24 16:43:53 +0600

    Add files via upload

---
+++ Lab Exam/storage/framework/views/2ae76f9c6ae1f481af4400fca9448a416fe8d851.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <div>
+        <h1>Welcome home!</h1>
+        <a href="<?php echo e(route('registration.emp')); ?>">Create New Employee</a> |
+        <a href="<?php echo e(route('home.emps')); ?>">View Employee List</a> |
+        <a href="<?php echo e(route('logout')); ?>">Logout</a>
+        <br>
+
+    </div>
+
+
+</body>
+
+</html><?php /**PATH D:\APWT\Final_Lab_Task-master\Final_Lab_Task-master\resources\views/home/adminhome.blade.php ENDPATH**/ ?>
