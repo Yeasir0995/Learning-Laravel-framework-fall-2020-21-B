917d91ca5fad15e987edd0db14f64fc5a2aa8b78
Yeasir0995 <40072145+Yeasir0995@users.noreply.github.com>
2020-12-27 23:35:10 +0600

    registration.blade updated

--- Lab Exam/resources/views/home/registration.blade.php
+++ Lab Exam/resources/views/home/registration.blade.php
@@ -10,13 +10,14 @@
             <fieldset>
                 <legend>Register Employee</legend>
                 <table>
-                    <tr>
-                        <td>Name</td>
-                        <td><input type="text" name="name"></td>
-                    </tr>
+                   
                     <tr>
                         <td>Username</td>
                         <td><input type="text" name="username"></td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td><input type="text" name="email"></td>
                     </tr>
                     <tr>
                         <td>Password</td>
